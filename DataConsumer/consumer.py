03969bfb09ad3e572ec48880e302d992775d7e54
OMID Ebrahimi <omidh225@gmail.com>
2024-11-06 06:10:23 +0330

    feat: add dockerfile consumer & edit consumer for dockerfile

--- DataConsumer/consumer.py
+++ DataConsumer/consumer.py
@@ -3,26 +3,36 @@ from datetime import datetime
 import psycopg2
 import redis
 from psycopg2 import Error
+import os
+
+
+user = os.environ["USERNAME"]
+password = os.environ["PASSWORD"]
+port_postgres = os.environ["PORT_POSTGRES"]
+host_postgres = os.environ["HOSTNAME"]
+database = os.environ["DB_NAME"]
+port_redis = os.environ["PORT_REDIS"]
+hostname_redis = os.environ["HOSTNAME_REDIS"]
 
 try:
     conn = psycopg2.connect(
-        user="postgres",
-        password="postgres",
-        port="54321",
-        host="127.0.0.1",
-        database="ecommerce_docker",
+        user=user,
+        password=password,
+        port=port_postgres,
+        host=host_postgres,
+        database=database,
     )
 
     cur = conn.cursor()
 
     create_table = """CREATE TABLE IF NOT EXISTS information (
-                                id SERIAL PRIMARY KEY,
-                                user_id INT NOT NULL,
-                                timestamp TIMESTAMP(0),
-                                gender VARCHAR(255),
-                                age INT,
-                                mark INT,
-                                diese VARCHAR(255));"""
+                    id SERIAL PRIMARY KEY,
+                    user_id INT NOT NULL,
+                    timestamp TIMESTAMP(0),
+                    gender VARCHAR(255),
+                    age INT,
+                    mark INT,
+                    diese VARCHAR(255));"""
     cur.execute(create_table)
     conn.commit()
     print("Table created successfully!")
@@ -30,13 +40,19 @@ try:
     insert_query = """INSERT INTO information (user_id, timestamp, gender, age, mark, diese)
                     VALUES (%s, %s, %s, %s, %s, %s);"""
 
-    r = redis.StrictRedis(host='localhost', port=6379)
+    r = redis.StrictRedis(host=hostname_redis, port=port_redis)
 
     while True:
         data = r.blpop("information")
         data_to_json = json.loads(data[1])
-        record_value = (data_to_json["user_id"], data_to_json["timestamp"], data_to_json["gender"],
-                        data_to_json["age"], data_to_json["mark"], data_to_json["diese"])
+        record_value = (
+            data_to_json["user_id"],
+            data_to_json["timestamp"],
+            data_to_json["gender"],
+            data_to_json["age"],
+            data_to_json["mark"],
+            data_to_json["diese"],
+        )
         print(data_to_json)
         cur.execute(insert_query, record_value)
 
